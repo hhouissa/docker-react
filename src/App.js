80a3b060bdeb27feb4cb4c8455ff9a917de81d5c
hhouissa <hhouissa1@gmail.com>
2018-12-02 00:56:40 +0100

    this is the branch feature 1st commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hihaaaa
+            this is the app js running on branch not master
           </p>
           <a
             className="App-link"
